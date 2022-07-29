07ed1feb966525dafce0062bb6e198cf3443d181
IagoLeoni <iagoleoni@gmail.com>
2022-07-29 01:49:59 +0000

    .

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -2,14 +2,14 @@
 # ACTION REQUIRED! Change "project-id-here" value to the project you'll be using
 export PROJECT_ID="gke-demo-cicd"
 # Test cluster
-echo "creating testcluster..."
-gcloud beta container --project "$PROJECT_ID" clusters create-auto "testcluster" \
+echo "creating devcluster..."
+gcloud beta container --project "$PROJECT_ID" clusters create-auto "devcluster" \
 --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Staging cluster
-echo "creating stagingcluster..."
-gcloud beta container --project "$PROJECT_ID" clusters create-auto "stagingcluster" \
+echo "creating qacluster..."
+gcloud beta container --project "$PROJECT_ID" clusters create-auto "qacluster" \
 --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,6 +1,6 @@
 # Initilize set project ID variable and run various initializations
 # ACTION REQUIRED! Change "project-id-here" value to the project you'll be using
-export PROJECT_ID="gke-demo-cicd"
+export PROJECT_ID="poc-devops-eqtl"
 # sets the current project for gcloud
 gcloud config set project $PROJECT_ID
 # Enables various APIs you'll need
@@ -8,7 +8,7 @@ gcloud services enable container.googleapis.com cloudbuild.googleapis.com \
 artifactregistry.googleapis.com clouddeploy.googleapis.com \
 cloudresourcemanager.googleapis.com
 # creates the Artifact Registry repo
-gcloud artifacts repositories create hello-world-python --location=us-central1 \
+gcloud artifacts repositories create containers-repo --location=us-central1 \
 --repository-format=docker
 # creates the Google Cloud Deploy pipeline
 gcloud beta deploy apply --file clouddeploy.yaml \
